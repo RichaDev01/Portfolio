a5dbe78c50b35dfb6cc1897ea00b24d68bc73516
Richa Garg <138007104+RichaDev01@users.noreply.github.com>
2023-10-01 20:01:35 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,36 +4,56 @@ import jobs from './pics/jobs.png';
 import mathQuiz from './pics/mathQuiz.png';
 import twiquer from './pics/twiquer.png';
 import mailgo from './pics/mailgo.png';
+import healthDesign from './pics/healthDesign.png';
+import gcmDesign from './pics/gcmDesign.png';
+import cibilDesign from './pics/cibilDesign.png';
 
 export const products = [
   {
     id: 1,
-    img: todo,
-    link: "https://cerulean-mousse-3c9bc1.netlify.app/",
+    img: healthDesign,
+    link: "https://www.figma.com/proto/ClUa3SVF3AtOgmBa4JS4rK/Health-Tracking-App?type=design&node-id=1-2062&t=gcvgD5skkVxmoxBB-0&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A2062",
   },
   {
     id: 2,
-    img: jobs,
-    link: "https://sage-zabaione-1900d7.netlify.app/",
+    img: cibilDesign,
+    link: "https://www.figma.com/proto/bVeKY4FC2ZZNI5a6ARhKtA/cibil-company-Website?type=design&node-id=109-180&t=xCmCYDn8UkuQhlnm-0&scaling=min-zoom&page-id=0%3A1",
   },
   {
     id: 3,
+    img: gcmDesign,
+    link: "https://www.figma.com/proto/YqfBxn1P16G9AW24fMdfNg/GCM-Corporate-Site-4.0?type=design&node-id=60-17734&t=WCz8O7KDCjusfUDI-0&scaling=min-zoom&page-id=0%3A1",
+  },
+  
+  {
+    id: 4,
     img: mathQuiz,
     link: "https://stellar-unicorn-857d2f.netlify.app/",
   },
   {
-    id: 4,
+    id: 5,
     img: mailgo,
     link: "https://github.com/RichaDev01/MailGo",
   },
   {
-    id: 5,
+    id: 6,
     img: twiquer,
     link: "https://ubiquitous-zuccutto-ba72fa.netlify.app/",
   },
   {
-    id: 6,
+    id: 7,
     img: meme,
     link: "https://resonant-sundae-169c4f.netlify.app/",
   },
+  {
+    id: 8,
+    img: todo,
+    link: "https://cerulean-mousse-3c9bc1.netlify.app/",
+  },
+  {
+    id: 9,
+    img: jobs,
+    link: "https://sage-zabaione-1900d7.netlify.app/",
+  },
+  
 ];
